883d6b43ab743d008ff8a54c5ae0f1a592c9b94a
Seth Holton <sethdholton@utexas.edu>
2024-09-30 11:48:31 -0500

    file upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# EUX_Project1
+It's a drum sequencer with a narrative element that draws a connection between developing a beat and my personal journey developing as a person.
+
+Code used:
+title: drum machine!
+author: asd0999
+link: https://editor.p5js.org/asd0999/sketches/r1od2atFQ
+
---
+++ sketch.js
@@ -0,0 +1,65 @@
+﻿let button;
+let particles;
+
+function setup() {
+  createCanvas(600, 600, WEBGL);
+
+  // Create a button to reset the particle system.
+  button = createButton('Reset');
+
+  // Call resetModel() when the user presses the button.
+  button.mousePressed(resetModel);
+
+  // Add the original set of particles.
+  resetModel();
+}
+
+function draw() {
+  background(50);
+
+  // Enable orbiting with the mouse.
+  orbitControl();
+
+  // Turn on the lights.
+  lights();
+
+  // Style the particles.
+  noStroke();
+
+  // Draw the particles.
+  model(particles);
+}
+
+function resetModel() {
+  // If the p5.Geometry object has already been created,
+  // free those resources.
+  if (particles) {
+    freeGeometry(particles);
+  }
+
+  // Create a new p5.Geometry object with random spheres.
+  particles = buildGeometry(createParticles);
+}
+
+function createParticles() {
+  for (let i = 0; i < 60; i += 1) {
+    // Calculate random coordinates.
+    let x = randomGaussian(0, 20);
+    let y = randomGaussian(0, 20);
+    let z = randomGaussian(0, 20);
+
+    push();
+    // Translate to the particle's coordinates.
+    translate(x, y, z);
+    // Draw the particle.
+    sphere(5);
+    pop();
+  }
+}
+
+function keyPressed() {
+  console.log(keyCode);
+  if (keyCode == 32) {
+    resetModel();
+  }
+}
